3064e6c32a8e0f67ec9625b12793396827c902a9
Leo <leo.cabral1016@gmail.com>
2020-12-26 15:33:45 -0300

    promises p2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -15,6 +15,7 @@ import { FormsModule } from '@angular/forms';
 import { ReactiveFormsModule } from '@angular/forms';
 import { MatSelectModule } from '@angular/material/select';
 import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 
 import { from } from 'rxjs';
 import 'hammerjs';
@@ -64,7 +65,8 @@ import { LoginComponent } from './components/login/login.component';
     MatInputModule,
     MatCheckboxModule,
     MatSelectModule,
-    MatSlideToggleModule
+    MatSlideToggleModule,
+    MatProgressSpinnerModule
   ],
   providers: [
     DishService,
